7cf1ba572ee577a3d2eb2ca21567d127db8fbe17
Moinwaqar <146329040+Moinwaqar@users.noreply.github.com>
2023-10-18 22:56:40 +0100

    #

---
+++ if_2.py
@@ -0,0 +1,14 @@
+age = int(input("What's your age?"))
+if age > 100:
+    print("you are very old,")
+    print("well done!")
+elif age > 80:
+    print("you are fairly old")
+    print("pretty good!")
+elif age > 40:
+    print("you are middle aged")
+    print("never mind")
+elif age > 30:
+    print("you are not very old - yet")
+else:
+    print("You're young")
